c463f74809e3a191f703ecff84feb512379f429f
Smit Baranwal <smitbaranwal@gmail.com>
2018-05-29 15:46:37 +0530

    Training partially done

--- src/app/pages/faq/faq.module.ts
+++ src/app/pages/faq/faq.module.ts
@@ -5,7 +5,6 @@ import {MatExpansionModule} from '@angular/material/expansion';
 import { FaqContainerComponent } from './faq-container/faq-container.component';
 import { FaqListComponent } from './faq-list/faq-list.component';
 import { FaqRoutingModule } from './faq-routing.module';
-import { SharedModule } from '../../@shared/shared.module';
 import { StoreModule } from '@ngrx/store';
 
 @NgModule({
